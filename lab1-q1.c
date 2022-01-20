e40040dd8bdacf3ad9dd132a685632b247383135
Fatih ORHAN <orhanfatih2002@gmail.com>
2022-01-20 15:20:49 -0500

    cleared memory leaks

--- lab1-q1.c
+++ lab1-q1.c
@@ -13,9 +13,17 @@ char * createDashes(int n) {
 }
 
 int main() { 
-    printf("%7s%16s%16s", "i", "sqrt(i)", "cbrt(i)\n");
-    printf("%s %s %s\n", createDashes(8), createDashes(15), createDashes(15));
+    char * eight;
+    eight = createDashes(8);
+    char * fifteen;
+    fifteen = createDashes(15);
+    
+    printf("%7s%16s %16s", "i", "sqrt(i)", "cbrt(i)\n");
+    printf("%s %s %s\n", eight, fifteen, fifteen);
     for (int i = 1; i < 31; i++) { 
         printf("%7d%16.9f%16.9f\n", i, pow(i, 0.5), pow(i, ((double) 1 / 3)));
     }
+    printf("%s %s %s\n", eight, fifteen, fifteen);
+    free(fifteen);
+    free(eight);
 }
