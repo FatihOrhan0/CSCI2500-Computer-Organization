0961383361ed375dcf353244a0a6002a2acb0bcb
Fatih ORHAN <orhanfatih2002@gmail.com>
2022-01-23 17:55:19 -0500

    working version of the first line printer

--- hw1/hw1.c
+++ hw1/hw1.c
@@ -20,14 +20,102 @@ int ** matrix_alloc(int rows, int cols) {
     return matrix;
 }
 
-//a function to print a matrix
-void printMatrix(int ** m, int row, int col) { 
+
+//this function finds the max digit number in a given matrix.
+int findMaxDigits(int ** m, const int row, const int col){ 
+    int max = 0;
     for (int i = 0; i < row; i++) { 
         for (int j = 0; j < col; j++) { 
-            printf("%d ", m[i][j]);
+            if (floor(log(m[i][j])) > max) { 
+                max = floor(log(m[i][j]));
+            }
+        }
+    }
+    return max;
+}
+
+//a function to print a matrix1 multiplied by matrix 2.
+void printMatrixes(int ** m1, int row1, int col1, int ** m2, int row2, int col2) { 
+    int max1 = findMaxDigits(m1, row1, col1);
+    int max2 = findMaxDigits(m2, row2, col2);
+
+    //first print the first rows of the matrixes
+    if (col1 == 1) { 
+        printf("[%*d", -max1, m1[0][0]);
+    }
+    else { 
+        printf("[");
+        for (int i = 0; i < col1; i++) { 
+            if (i < col1 - 1) { 
+                printf("%*d  ", -max1, m1[0][i]);
+            }
+            else { 
+                printf("%d", m1[0][i]);
+            }
+        }
+    }
+    printf("] multiplied by ");
+
+    if (col2 == 1) { 
+        printf("[%*d", -max2, m2[0][0]);
+    }
+    else { 
+        printf("[");
+        for (int i = 0; i < col2; i++) { 
+            if (i < col2 - 1) { 
+                printf("%*d  ", -max2, m2[0][i]);
+            }
+            else { 
+                printf("%d", m2[0][i]);
+            }
+        }
+    }
+    printf("]");
+    
+    //at this point the first line is done. 
+    printf("\n");
+
+    //create two booleans to keep track of the rows of matrixes.
+    bool first = true;
+    bool second = true;
+    int rowFirst = 1; 
+    int rowSecond = 1;
+    while (true) { 
+        if (!first || !second) break;
+        if (rowFirst == row1) { 
+            first = false;
+            continue;
+        }
+        if (rowSecond == row2) { 
+            second = false;
+            continue;
+        }
+
+        if (col1 == 1) { 
+            printf("[%d]", m1[rowFirst][0]);
+        }
+        else { 
+            printf("[");
+            for (int i = 0; i < col1; i++) { 
+                if (i < col1 - 1) { 
+                    printf("%*d  ", -max1, m1[rowFirst][i]);
+                }
+                else { 
+                    printf("%d", m1[rowFirst][i]);
+                }
+            }
+        }
+        printf("%-14c", ']');
+        rowFirst++;
+        rowSecond++;
+    }
+
+    /* for (int i = 0; i < row1; i++) { 
+        for (int j = 0; j < col1; j++) { 
+            printf("%d ", m1[i][j]);
         }
         printf("\n");
-    }
+    } */
 }
 
 int main(int argc, char * argv[]) {
@@ -36,6 +124,7 @@ int main(int argc, char * argv[]) {
         fprintf(stderr, "ERROR: Invalid inputs!\n");
         return EXIT_FAILURE;
     }
+
     //check if any of the values are negative
     bool neg = false;
     for (int i = 1; i < argc; i++) { 
@@ -77,7 +166,7 @@ int main(int argc, char * argv[]) {
             matrix2[i][j] = read;
         }
     }
-
+    printMatrixes(matrix1, atoi(argv[1]), atoi(argv[2]), matrix2, atoi(argv[3]), atoi(argv[4]));
 
     return EXIT_SUCCESS;
 } 
