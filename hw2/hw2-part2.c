13103ca2204d7d434d8dd80660277fb7954a88ef
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-02-08 16:34:43 -0500

    completed the processing of the first file

--- hw2/hw2-part2.c
+++ hw2/hw2-part2.c
@@ -10,6 +10,31 @@
 //TODO: Parse the left of the first file. 
 //start working on the second file.
 
+//this function reads the first n characters and stores them in line
+void readLine(char * line, FILE * file1, int n) { 
+    int i = 0;
+    char c;
+    do { 
+        c = fgetc(file1);
+        if (isprint(c)) { 
+            line[i] = c;
+        }
+        else { 
+            line[i] = '\0';
+            break; }
+        if (c == '\n') { 
+            line[i] = '\0';
+            break; }
+        i++;
+    } while (i < n);
+    line[n] = '\0';
+
+    //read the rest of the line
+    while (c != '\n') { 
+        c = fgetc(file1);
+    }
+}
+
 //this function processes the first file
 int processFirst(char * filename, char * line, int * cols, char * letter) { 
     //open the file
@@ -18,14 +43,10 @@ int processFirst(char * filename, char * line, int * cols, char * letter) {
         fprintf(stderr, "ERROR: cannot open %s.", filename);
         return EXIT_FAILURE;
     }
-    fgets(line, 33, file1);
-    //if there exists a trailing new line character, get rid of it.
-    for (int i = 0; i < 33; i++) { 
-        if (line[i] == '\n') { 
-            line[i] = '\0';
-            break;
-        }
-    }
+    //read the first 32 characters (if enough) to line
+    readLine(line, file1, 32);
+
+    printf("%s", line);
     //read the number of columns in colNumber and parse to int
     char * charCols = malloc(4 * sizeof(char));
     fgets(charCols, 4, file1);
@@ -35,6 +56,8 @@ int processFirst(char * filename, char * line, int * cols, char * letter) {
         fprintf(stderr, "ERROR: Expected a Non-Zero Number on The Second Line.\n");
         return EXIT_FAILURE;
     }
+
+    //get the printable character in the next line
     * letter = fgetc(file1);
     if (fgetc(file1) != '\n') { 
         fprintf(stderr, "ERROR: Invalid Character.");
@@ -44,7 +67,20 @@ int processFirst(char * filename, char * line, int * cols, char * letter) {
         fprintf(stderr, "ERROR: Invalid Character.");
         return EXIT_FAILURE;
     }
-    printf("%c", *letter);
+    printf("%c %d\n", *letter, *cols);
+    
+    //allocate memory to store the columns
+    char ** colData = malloc((*cols) * sizeof(char*));
+    for (int i = 0; i < (*cols); i++) { 
+        colData[i] = malloc(17 * sizeof(char));
+    }
+
+    for (int i = 0; i < (*cols); i++) { 
+        readLine(colData[i], file1, 16);
+    }
+    for (int i = 0; i < (*cols); i++) { 
+        printf("%s\n", colData[i]);
+    } 
     return EXIT_SUCCESS;
 }
 
@@ -55,7 +91,7 @@ int main (int argc, char * argv[]) {
         return EXIT_FAILURE;
     }
     int * cols = malloc(sizeof(int));
-    char line[32];
+    char line[33];
     char letter;
     processFirst(argv[1], line, cols, &letter);
     free(cols);
