cf0be719aa8587a3eb4c4422407f7f5bb1fdc379
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-02-13 17:08:28 -0500

    converted to pointer arithmetic

--- test1/strcat.c
+++ test1/strcat.c
@@ -17,18 +17,18 @@ char * strcat(char * s1, char * s2) {
     }
     char * res = malloc((len1 + len2 + 1) * sizeof(char));
     for (int i = 0; i < len1; i++) { 
-        res[i] = s1[i];
+        *(res + i) = *(s1 + i);
     }
     for (int i = len1; i < len1 + len2; i++) { 
-        res[i] = s2[i - len1];
+        *(res + i) = *(s2 + i - len1);
     }
-    res[len1 + len2] = 0;
+    *(res + len1 + len2) = 0;
     s1 = res;
     return s1;
 }
 
 int main() {
-    char * a = NULL;    
+    char * a = "abc";    
     char * b = NULL;    
     printf("%s\n", strcat(a, b));
 }
