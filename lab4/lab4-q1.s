399dc5986e12eb837e4487c7aa1266a433586996
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-03-14 19:06:41 -0400

    final version of the first checkpoint

--- lab4/lab4-q1.s
+++ lab4/lab4-q1.s
@@ -1,40 +1,35 @@
 
 .data
-  even:     .asciiz "EVEN\n"
-  odd:      .asciiz "ODD\n"
-
-
+  even: .asciiz "EVEN\n" "ODD\n"
 .text
   .globl main  
-
 main: 
     sub $sp, $sp, 4
     sw $ra, 0($sp)
-    addi $a0, $a0, 1071
+    addi $a0, $a0, 1072
     jal parity
-    add $a0, $s0, $0
-    beq $v0, 0, printEven
-    li $v0, 1
-    syscall
     lw  $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
-
-    printEven: 
-    li $v0, 4
-    la $a0, even
-    syscall
-    lw  $ra, 0($sp)
-    addi $sp, $sp, 4
-    jr $ra
-
-
-
 
 parity: 
     add $t0, $a0, $0
     sll $s0, $t0, 31
     srl $s0, $s0, 31
+    beq $a1, 0, exit
+
+    addi $t3, 6
+    mult $s0, $t3
+    mflo $s0
+    
+    printEven: 
+    li $v0, 4
+    la $a0, even
+    add $a0, $a0, $s0
+    syscall
+
+    exit: 
+    move $v0, $s0
     jr $ra
 
 
