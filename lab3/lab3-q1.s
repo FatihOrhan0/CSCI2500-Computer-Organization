07e676812d6036bfa410f1ec413368d9a231d1f8
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-03-14 11:54:00 -0400

    formatting

--- lab3/lab3-q1.s
+++ lab3/lab3-q1.s
@@ -6,32 +6,32 @@
   .globl main
 
 main:   
-        sub $sp, $sp, 4   # allocate stack space to store $ra
-        sw  $ra, 0($sp)   # store $ra on the stack
+  sub $sp, $sp, 4   # allocate stack space to store $ra
+  sw  $ra, 0($sp)   # store $ra on the stack
 
 
-        li $s0, -20             # Load Immediate (li)  lucky = 13
-        sgt $t0, $s0, -13       #check if s0 > -13, store in t0
-        beq $t0, 1, assign_tag  #forward to assign_tag if t0 == 1
-        beq $t0, 0, normal_order
+  li $s0, -20             # Load Immediate (li)  lucky = 13
+  sgt $t0, $s0, -13       #check if s0 > -13, store in t0
+  beq $t0, 1, assign_tag  #forward to assign_tag if t0 == 1
+  beq $t0, 0, normal_order
 
-        assign_tag: 
-            sub $s0, $s0, 6     #lucky -= 6;
-        
-        normal_order:
-        li $v0, 4
-        la $a0, string
-        syscall
-        li $v0, 1
-        add $a0, $s0, 0
-        syscall
-        li $v0, 4
-        la $a0, newline
-        syscall
+  assign_tag: 
+    sub $s0, $s0, 6     #lucky -= 6;
+  
+  normal_order:
+  li $v0, 4
+  la $a0, string
+  syscall
+  li $v0, 1
+  add $a0, $s0, 0
+  syscall
+  li $v0, 4
+  la $a0, newline
+  syscall
 
-        #li $v0, 1
-        #syscall
+  #li $v0, 1
+  #syscall
 
-        lw  $ra, 0($sp)
-        add $sp, $sp, 4
-        jr $ra
+  lw  $ra, 0($sp)
+  add $sp, $sp, 4
+  jr $ra
