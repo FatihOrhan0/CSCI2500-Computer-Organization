92a5863d0c74ae4ff3b0e696c242c638170d6915
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-03-14 23:58:52 -0400

    final version with both extract bytes functions

--- lab4/lab4-q2.s
+++ lab4/lab4-q2.s
@@ -4,13 +4,17 @@
   three: .asciiz "| byte #3: "
   four: .asciiz "| byte #4: "
   value: .word 0x4a889cf1
+  first: .word 0xff000000
+  second: .word 0x00ff0000
+  third: .word 0x0000ff00
+  fourth: .word 0x000000ff
 .text
   .globl main  
 main: 
     sub $sp, $sp, 4
     sw $ra, 0($sp)
     lw $a0, value
-    jal extractBytes
+    jal extractBytes2
     lw $ra, 0($sp)
     addi $sp, $sp, 4
     jr $ra
@@ -61,4 +65,46 @@ extractBytes:
 
     jr $ra
 
+extractBytes2:
+    move $s0, $a0
+    lw $s1, first
+    and $t0, $a0, $s1
+    srl $t0, $t0, 24
+    lw $s1, second
+    and $t1, $a0, $s1
+    srl $t1, $t1, 16
+    lw $s1, third
+    and $t2, $a0, $s1
+    srl $t2, $t2, 8
+    lw $s1, fourth
+    and $t3, $a0, $s1
+   
+    la $a0, one
+    li $v0, 4
+    syscall
+    add $a0, $t0, 0
+    li $v0, 1
+    syscall
+
+    la $a0, two
+    li $v0, 4
+    syscall
+    li $v0, 1
+    add $a0, $t1, $0
+    syscall
+
+    la $a0, three
+    li $v0, 4
+    syscall
+    li $v0, 1
+    add $a0, $t2, $0
+    syscall
+
+    la $a0, four
+    li $v0, 4
+    syscall
+    li $v0, 1
+    add $a0, $t3, $0
+    syscall
     
+    jr $ra
