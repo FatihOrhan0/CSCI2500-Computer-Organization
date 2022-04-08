eb3288dbcc39b5fe483fea4554b8106868c5722f
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-04-08 04:02:05 -0400

    Commented the purpose of the file

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -1,3 +1,7 @@
+//Filename: hw5.c
+//Purpose: Implementation file for the 5th homework. The aim is to create a 
+//rudimentary compiler that can compile addition and subtraction into c code.
+
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
