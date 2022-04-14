21ff071d06aeb37cc745ef71a9acc089f6f2cfe1
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-04-14 01:41:33 -0400

    rudimentary parts are done

--- hw5/hw5.c
+++ hw5/hw5.c
@@ -7,7 +7,24 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+//define the T registers in a string 
+char * regs[18] = {"t0", "t1", "t2", "t3", "t4", "t5", "t6", "t7", "t8", "t9",
+ "s0", "s1", "s2", "s3", "s4", "s5", "s6", "s7"};
 
-int main() { 
-    return 0;
+
+
+int main(int argc, char * argv[]) { 
+    if (argc != 2) { 
+        fprintf(stderr, "ERROR: Incompatible Arguments!\n");
+        return EXIT_FAILURE;
+    }
+    char * buffer = calloc(sizeof(char), 300);
+    FILE * file = fopen(argv[1], "r");
+    while ((buffer = fgets(buffer, 100, file))) { 
+        for (unsigned int i = 0; i < sizeof(buffer); i++) { 
+            printf("%c", buffer[i]);
+        }
+        printf("%s", buffer); 
+    }
+    return EXIT_SUCCESS;
 }
