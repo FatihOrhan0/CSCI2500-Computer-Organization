8fdf05a71002d1a37a54150a717b63e43c44639e
FatihOrhan0 <orhanfatih2002@gmail.com>
2022-04-27 17:56:48 -0400

    dealt with the first line

--- hw6/hw6.c
+++ hw6/hw6.c
@@ -8,12 +8,58 @@
 #include <unistd.h>
 #include <string.h>
 
+//TODO: 
 
+
+//refined strlen function
+unsigned int strlen2(char * s) { 
+    return 2;
+}
 
 
 int main(int argc, char * argv[]) { 
+    if (argc != 2 && argc != 3) {
+        fprintf(stderr, "ERROR: INCOMPATIBLE COMMAND LINE ARGUMENTS!\n");
+        return EXIT_FAILURE;
+    }
+    
+    //this array will be used to store the locations of integers
+    int letters[26];
+
+
+    //initialize the letters array.
+    for (int i = 0; i < 26; i++) { 
+        letters[i] = -1;
+    }
+
+    //check pipelining
+    short isPipe = 0; 
+    if (argc == 3) {  
+        if (strcmp("--pipelined", argv[2]) == 0) { 
+            isPipe = 1;
+        }
+    }
+
+    //open the file for read
+    FILE * file = fopen(argv[1], "r");
+
+    //store the integers in the stack
+    char buffer[100];
+    fgets(buffer, 100, file); 
+    int counter = 0;
+    for (int i = 4; i < (int) strlen(buffer) - 1; i += 3) { 
+        letters[buffer[i] - 'a'] = counter * 4;
+        counter++;
+    }
+    
+
     if (argc > 1) { 
         printf("%s\n", argv[1]);
     }
+
+
+
+    //close the file and exit the program
+    fclose(file);
     return EXIT_SUCCESS;
 }
